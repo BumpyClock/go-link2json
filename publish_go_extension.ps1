e824981ef4bdeaba89d0f9a000a9541c3897fdaa
Aditya Sharma <adityaksharma@gmail.com>
2024-10-01 13:28:20 -0700

    chore: update version to v1.0.10 and add module listing for go-link2json

--- publish_go_extension.ps1
+++ publish_go_extension.ps1
@@ -1,4 +1,5 @@
 go mod tidy
-git commit -m "go-link2json: changes for v1.0.1"
-git tag v1.0.1
-git push origin v1.0.1
+git commit -m "go-link2json: changes for v1.0.10"
+git tag v1.0.10
+git push origin v1.0.10
+go list -m github.com/BumpyClock/go-link2json
