cd0246d854a5305bf9367c792dfe49ba8092ca26
bumpyclock <adityaksharma@gmail.com>
2024-12-05 06:57:56 -0800

    go-link2json: changes for v1.0.12

--- publish_go_extension.ps1
+++ publish_go_extension.ps1
@@ -1,5 +1,7 @@
+$env:GOPROXY = "https://proxy.golang.org"
 go mod tidy
-git commit -m "go-link2json: changes for v1.0.11"
-git tag v1.0.11
-git push origin v1.0.11
+git add *
+git commit -m "go-link2json: changes for v1.0.12"
+git tag v1.0.12
+git push origin v1.0.12
 go list -m github.com/BumpyClock/go-link2json
